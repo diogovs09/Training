475e84a361e5114b7b176bc300552ddbf51d098d
diogo_vsilva <divsilva09@gmail.com>
2024-07-31 14:46:05 +0100

    3

---
+++ eclipse-workspace/CognizantRevEng/src/testing_javatpoint/StaticBinding.java
@@ -0,0 +1,10 @@
+package testing_javatpoint;
+
+class Dog{  
+	 private void eat(){System.out.println("dog is eating...");}  
+	  
+	 public static void main(String args[]){  
+	  Dog d1=new Dog();  
+	  d1.eat();  
+	 }  
+	}  
