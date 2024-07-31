91e9db2fa2e6145d42c74e5f9df528c20c939beb
diogo_vsilva <divsilva09@gmail.com>
2024-07-31 16:15:01 +0100

    3

--- android-studio/cognizantandroidrever/app/src/main/java/com/example/cognizantrever/MainActivity.kt
+++ android-studio/cognizantandroidrever/app/src/main/java/com/example/cognizantrever/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.cognizantrever
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.AlarmClock
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +24,37 @@ class MainActivity : AppCompatActivity() {
         var nameEditText : EditText = findViewById(R.id.etName)
         var mainTextView : TextView = findViewById(R.id.tvMain)
 
-        var data = nameEditText.text.toString()
+        var data = nameEditText.text.toString();
         mainTextView.setText(data)
 
+        var hIntention = Intent(this,HomeActivity::class.java)
+        hIntention.putExtra("mykey",data)
+        startActivity(hIntention)
+
+    }
+
+    fun openDialer(view: View) {
+        var dialerIntention = Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678"))
+        startActivity(dialerIntention)
+    }
+
+    fun setAlarm(view: View) {
+        createAlarm("cognizantrev",20,43)
+    }
+
+    fun createAlarm(message: String, hour: Int, minutes: Int) {
+        val intent = Intent(AlarmClock.ACTION_SET_ALARM).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE, message)
+            putExtra(AlarmClock.EXTRA_HOUR, hour)
+            putExtra(AlarmClock.EXTRA_MINUTES, minutes)
+        }
+        //if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        //}
+    }
+
+    fun openMyCalendar(view: View) {
+        var calIntent = Intent("cognizant.portugal.android")
+        startActivity(calIntent)
     }
 }
