0c2d40292ebabbc9c8c97a1b58daa539b78cc707
diogo_vsilva <divsilva09@gmail.com>
2024-08-05 17:48:18 +0100

    4

---
+++ android-studio/NotificationScheduler/app/src/main/java/com/example/notificationscheduler/NotificationJobService.java
@@ -0,0 +1,37 @@
+package com.example.notificationscheduler;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationJobService extends JobService {
+
+    @Override
+    public boolean onStartJob(JobParameters jobParameters) {
+        //Set up the notification content intent to launch the app when clicked
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MainActivity.class),
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager manager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setContentTitle("Job Service")
+                .setContentText("Your job is running!")
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+        manager.notify(0, builder.build());
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters jobParameters) {
+        return true;
+    }
+}
---
+++ eclipse-workspace/CognizantRevEng/src/javatpoint_multithreading/ThreadJoin.java
@@ -0,0 +1,32 @@
+package javatpoint_multithreading;
+
+//A Java program for understanding   
+//the joining of threads  
+
+//import statement  
+import java.io.*;  
+
+//The ThreadJoin class is the child class of the class Thread  
+class ThreadJoin extends Thread  
+{  
+//overriding the run method  
+public void run()  
+{  
+for (int j = 0; j < 2; j++)  
+{  
+try  
+{  
+//sleeping the thread for 300 milli seconds  
+Thread.sleep(300);  
+System.out.println("The current thread name is: " + Thread.currentThread().getName());  
+}  
+//catch block for catching the raised exception  
+catch(Exception e)  
+{  
+System.out.println("The exception has been caught: " + e);  
+}  
+System.out.println( j );  
+}  
+}  
+}  
+
