d9b18908cf0a3b8ec6c950cbec15c90713d8088f
diogo_vsilva <divsilva09@gmail.com>
2024-09-02 17:23:02 +0100

    AccessibilityMalware
    test

---
+++ android-studio/SensorSurvey/app/src/main/java/com/example/sensorsurvey/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.sensorsurvey;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+
+    // Individual light and proximity sensors.
+    private Sensor mSensorProximity;
+    private Sensor mSensorLight;
+
+    // TextViews to display current sensor values
+    private TextView mTextSensorLight;
+    private TextView mTextSensorProximity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Sensor code
+        mTextSensorLight = (TextView) findViewById(R.id.label_light);
+        mTextSensorProximity = (TextView) findViewById(R.id.label_proximity);
+
+        mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        String sensor_error = getResources().getString(R.string.error_no_sensor);
+
+        if (mSensorLight == null) {
+            mTextSensorLight.setText(sensor_error);
+        }
+        if (mSensorProximity == null) {
+            mTextSensorProximity.setText(sensor_error);
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mSensorProximity != null) {
+            mSensorManager.registerListener(this, mSensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorLight != null) {
+            mSensorManager.registerListener(this, mSensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        SensorEvent event = null;
+        int sensorType = event.sensor.getType();
+        float currentValue = event.values[0];
+        switch (sensorType) {
+            // Event came from the light sensor.
+            case Sensor.TYPE_LIGHT:
+                mTextSensorLight.setText(getResources().getString(R.string.label_light, currentValue));
+                // Handle light sensor
+                break;
+            case Sensor.TYPE_PROXIMITY:
+                mTextSensorProximity.setText(getResources().getString(R.string.label_proximity, currentValue));
+                break;
+            default:
+                // do nothing
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
